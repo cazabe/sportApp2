bf7c1452ed3c33561d2e079f7d453b0c8038bc8c
carlos zavala <cazcarlos@hotmail.com>
2020-08-05 17:06:41 -0500

    finishing register user controller , encrypting password

--- backend/src/controllers/User.controller.js
+++ backend/src/controllers/User.controller.js
@@ -1,18 +1,26 @@
-const User = require('../models/User.model')
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async createUser(req ,res){
         try {
            const {firstName , lastName ,password,email} = req.body;
 
-           const user = await User.create({
-            firstName:firstName,
-             lastName:lastName,
-             password:password, 
-             email:email
-           })
+           const existenUser = await User.findOne({email});
 
-           return res.json(user);
+           if(!existenUser){
+               const hashedPassword = await bcrypt.hash(password , 10) 
+               const user = await User.create({
+                 firstName,
+                 lastName,
+                 email,
+                 password:hashedPassword
+               })
+    
+               return res.json(user);
+           }else{
+               return res.status(400).json({message: "User already exist, do you whant to login?"});
+           }
           
         } catch (error) {
             throw Error(`Error while registering a new user ${error}`);
